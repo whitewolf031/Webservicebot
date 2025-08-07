2d4737928a45f08ad0f33c9f3aa2b64f3d130fa0
Sardorbek031 <sardorbekergashev031@gmail.com>
2025-08-07 16:34:50 +0500

    change the config

--- config.py
+++ config.py
@@ -1,17 +1,39 @@
 from dotenv import load_dotenv
 import os
+import urllib.parse as up
 
 class Config:
     def __init__(self):
         load_dotenv()
-        self.db_bot()
-        self.getBotEnv()
+        self.set_database_config()
+        self.set_bot_config()
+        self.set_url_config()
 
-    def getBotEnv(self):
-        self.token = os.getenv("TOKEN", "deafulttoken")
+    def set_database_config(self):
+        # Agar DATABASE_URL mavjud bo‘lsa, undan foydalanamiz
+        database_url = os.getenv("DATABASE_URL")
+        if database_url:
+            up.uses_netloc.append("postgres")
+            url = up.urlparse(database_url)
+            self.host = url.hostname
+            self.user = url.username
+            self.password = url.password
+            self.db = url.path[1:]
+            self.port = url.port
+        else:
+            # Aks holda alohida maydonlardan olamiz
+            self.host = os.getenv("HOST", "localhost")
+            self.user = os.getenv("USER", "postgres")
+            self.password = os.getenv("PASSWORD", "123456")
+            self.db = os.getenv("DATABASE", "mydb")
+            self.port = int(os.getenv("PORT", 5432))
 
-    def db_bot(self):
-        self.host = os.getenv("HOST", "localhost")
-        self.user = os.getenv("USER", "postgres")
-        self.db = os.getenv("DATABASE", "webcervice")
-        self.password = os.getenv("PASSWORD", "123456")
+    def set_bot_config(self):
+        self.token = os.getenv("TOKEN", "defaulttoken")
+        self.canal_id = os.getenv("CANAL_ID")
+        self.owner_id = os.getenv("OWNER_ID")
+
+    def set_url_config(self):
+        self.instagram_url = os.getenv("INSTAGRAM_URL")
+        self.youtube_url = os.getenv("YOUTUBE_URL")
+        self.telegram_url = os.getenv("TELEGRAM_URL")
